2730d8102a30f6582b5654550aa1491530c65d46
Duncan Sterken <duncte123@users.noreply.github.com>
2017-08-26 14:57:00 +0200

    Change Scheduler to use Timer

--- src/main/java/ml/duncte123/videobot/utils/Scheduler.java
+++ src/main/java/ml/duncte123/videobot/utils/Scheduler.java
@@ -4,55 +4,34 @@ import ml.duncte123.videobot.Video;
 import ml.duncte123.videobot.Videobot;
 import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Message;
+import org.apache.commons.lang3.time.DateUtils;
 
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
-import static java.util.Calendar.DAY_OF_WEEK;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Scheduler  {
 
     public Scheduler() {
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(13);
-        Date aDate = new Date();
-        Calendar with = Calendar.getInstance();
-        with.setTime(aDate);
+        Timer timer = new Timer();
 
-        int hour = with.get(Calendar.HOUR_OF_DAY);
-        int intDelayInHour = hour < 5 ? 5 - hour : 24 - (hour - 5);
 
-        System.out.println("Current Hour: " + hour);
-        System.out.println("Comuted Delay for next 1 PM: " + intDelayInHour);
-
-        Map<Integer, Integer> dayToDelay = new HashMap<>();
-        dayToDelay.put(Calendar.FRIDAY, 5);
-        dayToDelay.put(Calendar.SATURDAY, 4);
-        dayToDelay.put(Calendar.SUNDAY, 3);
-        dayToDelay.put(Calendar.MONDAY, 2);
-        dayToDelay.put(Calendar.TUESDAY, 1);
-        dayToDelay.put(Calendar.WEDNESDAY, 0);
-        dayToDelay.put(Calendar.THURSDAY, 6);
-        int dayOfWeek = with.get(DAY_OF_WEEK);
-        int delayInDays = dayToDelay.get(dayOfWeek);
-
-        scheduler.scheduleAtFixedRate(() -> {
-            try {
-                Video v = YoutubeUtil.getVideo();
-                if(YoutubeUtil.checkNewVideo(v)) {
-                    Message m = new MessageBuilder().append("Duncte uploaded a new video").setEmbed(EmbedUtil.generateEmbed(v)).build();
-                    Videobot.dhgGuild.getTextChannelsByName(Videobot.AD_CHANNEL_NAME, true)
-                            .get(0).sendMessage(m).queue();
+        TimerTask myTask = new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    Video v = YoutubeUtil.getVideo();
+                    if (YoutubeUtil.checkNewVideo(v)) {
+                        Message m = new MessageBuilder().append("Duncte uploaded a new video").setEmbed(EmbedUtil.generateEmbed(v)).build();
+                        Videobot.dhgGuild.getTextChannelsByName(Videobot.AD_CHANNEL_NAME, true)
+                                .get(0).sendMessage(m).queue();
+                    }
+                } catch (Exception ex) {
+                    ex.printStackTrace(); // or loggger would be better
                 }
-            } catch (Exception ex) {
-                ex.printStackTrace(); // or loggger would be better
             }
-        }, delayInDays, 7, TimeUnit.DAYS);
+        };
+        timer.schedule(myTask, DateUtils.MILLIS_PER_HOUR, DateUtils.MILLIS_PER_HOUR);
     }
-    }
+}
 
 
